942bcb2bae63d241d0858a3ed5cb0f5729770b2e
ComputerChip16 <159206470+ComputerChip16@users.noreply.github.com>
2024-03-11 14:13:50 +1100

    Number Game Frame

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let myIntro = 2;
-let enterName = prompt = 1;
+let enterName = prompt("Enter Your Name");
+let playerGuess;
+let playerFeedback
+loadDIV ();
 
-if (myIntro == enterName); {
-alert (myIntro);
+function loadDIV () {
+document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1><H1>Geese a numero between uno and 1</H2>"
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Geese a numero between uno and 1"
+do  cument.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML
+}
+
+function enterNumber () {
+    playerGuess = prompt ("entree a numero")
 }
