fe8602ef8c9955e503da658daaa2f2bcd4d4ec2e
ComputerChip16 <159206470+ComputerChip16@users.noreply.github.com>
2024-03-13 14:21:14 +1100

    do cument fix

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ loadDIV ();
 function loadDIV () {
 document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1><H1>Geese a numero between uno and 1</H2>"
 document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Geese a numero between uno and 1"
-do  cument.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML
 }
 
 function enterNumber () {
