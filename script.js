9c0920cade0ecd81c0ccbff19ee63ecfea5f2638
ComputerChip16 <159206470+ComputerChip16@users.noreply.github.com>
2024-03-13 15:00:47 +1100

    code update

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let enterName = prompt("Enter Your Name");
 let playerGuess;
+let computerGuess = randomNum();
 let playerFeedback
 loadDIV ();
+
+function randomNum() {
+    return Math.floor(Math.random() * (10 - 1 + 1) + 1)
+}
 
 function loadDIV () {
 document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1><H1>Geese a numero between uno and 1</H2>"
 document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Geese a numero between uno and 1"
-document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback
+
 }
 
 function enterNumber () {
